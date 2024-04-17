c756089e3f0e9f4836c88b946b71d8de4cffdc58
William <williamenglish1220@gmail.com>
2024-04-17 16:29:13 -0400

    Added ImGui Code

--- CatEngine/src/main.cpp
+++ CatEngine/src/main.cpp
@@ -1,3 +1,5 @@
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_glfw_gl3.h"
 #include<glad/glad.h>
 #include <GLFW/glfw3.h>
 
@@ -15,6 +17,8 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
+
+
 int main(void)
 {
     GLFWwindow* window;
@@ -23,20 +27,27 @@ int main(void)
     if (!glfwInit())
         return -1;
 
-    glfwSwapInterval(1);
+    
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(500, 500, "Pig!", NULL, NULL);
+    window = glfwCreateWindow(1920, 1080, "Pig!", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
-
+    glfwSwapInterval(1);
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
     gladLoadGL();
+
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io; 
+
+    ImGui::StyleColorsDark();
+    
+    ImGui_ImplGlfwGL3_Init(window, true);
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
@@ -75,7 +86,9 @@ int main(void)
     texture.Bind();
     shader.SetUniform1i("u_Texture", 0);
 
-
+    bool show_demo_window = true;
+    bool show_another_window = true;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     shader.Unbind();
     vb.Unbind();
@@ -84,17 +97,65 @@ int main(void)
     Renderer renderer;
     while (!glfwWindowShouldClose(window))
     {
+        glfwPollEvents();
         /* Render here */
         renderer.Clear();
 
         renderer.Draw(va, ib, shader);
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+
+        ImGui_ImplGlfwGL3_NewFrame();
+
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            ImGui::End();
+        }
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
 
         /* Poll for and process events */
-        glfwPollEvents();
-    }
+        
 
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+
+    }
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
