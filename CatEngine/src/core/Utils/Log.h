19e83ae15e3414c5a2536fa51e924a0985e88fa5
William <williamenglish1220@gmail.com>
2024-04-18 08:32:52 -0400

    Added a couple debug functions LogFloat1, LogFloat2, LogString

--- CatEngine/src/core/Utils/Log.h
+++ CatEngine/src/core/Utils/Log.h
@@ -8,6 +8,13 @@ void GLClearError();
 bool GLLog(const char* function, const char* file, int line);
 
 class Log {
+private:
+	bool m_SingleUse;
+	bool m_IsCalled;
 public:
-	
+	static void LogString(const char* str0);
+	static void LogFloat1(const char* des0, const float v0);
+	static void LogFloat2(const char* des0, const float v0, const char* des1, const float v1);
+
+
 };
